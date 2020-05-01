1c8b8c0035d961b3812b2da6057153265cfb12c2
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2020-04-30 20:37:43 -0500

    Starter Files

---
+++ student_code.py
@@ -0,0 +1,8 @@
+import common
+def astar_search(map):
+	found = False
+	# PUT YOUR CODE HERE
+	# access the map using "map[y][x]"
+	# y between 0 and common.constants.MAP_HEIGHT-1
+	# x between 0 and common.constants.MAP_WIDTH-1
+	return found
